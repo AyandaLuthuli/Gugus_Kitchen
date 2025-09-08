aa386d50009f8f10cf7cd33b0bf71a499bf6b186
AyandaLuthuli <ayandaalton2003@gmail.com>
2025-09-08 17:34:27 +0200

    GuestMenuBTN fix

--- inventory.js
+++ inventory.js
@@ -348,6 +348,8 @@ if (!user || (user.role !== "admin" && user.role !== "cashier")) {
 // // =============================
 // Supabase Initialization
 // =============================
+
+////////////////////////////////////////////////////////////
 const SUPABASE_URL = "https://dqnmbayimyiuqfzdalox.supabase.co";
 const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRxbm1iYXlpbXlpdXFmemRhbG94Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ5OTIwOTYsImV4cCI6MjA3MDU2ODA5Nn0.XPsThMYTgbUX4JU743QhdDwPvVMcPZ1_3OBvMMGW5o4";
